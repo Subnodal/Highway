2784f291c13a07f12f344040837eb7e76b26d0e5
James-Livesey <jamesliveg@gmail.com>
2024-12-03 18:49:37 +0000

    Add response deserialiser

--- src/main.js
+++ src/main.js
@@ -18,8 +18,41 @@ async function serialiseRequest(request) {
     return data;
 }
 
+function deserialiseResponse(data) {
+    var text = new TextDecoder("ascii").decode(data);
+    var bodyOffset = text.split("\r\n\r\n")[0].length + 4;
+    var lines = text.split("\r\n");
+    var statusLine = lines.shift();
+    var headers = new Headers();
+    var statusMatch;
+    var status = null;
+    var statusText = null;
+
+    if (statusMatch = statusLine?.match(/HTTP\/\d+(?:\.\d+) (\d+) (.+)/)) {
+        status = Number(statusMatch[1]);
+        statusText = statusMatch[2];
+    }
+
+    for (var line of lines) {
+        if (line == "") {
+            break;
+        }
+
+        var parts = line.split(": ");
+
+        headers.append(parts[0], parts[1]);
+    }
+
+    return new Response(data.slice(bodyOffset), {status, statusText, headers});
+}
+
 Deno.serve({port: Number(args.port)}, async function(request) {
     console.log(new TextDecoder().decode(await serialiseRequest(request)));
 
-    return new Response("OK");
+    return deserialiseResponse(new TextEncoder().encode(`HTTP/1.1 200 OK\r
+Content-Length: 13\r
+Content-Type: text/plain\r
+\r
+Hello, world!
+`));
 });
