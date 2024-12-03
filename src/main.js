18e328735491cc8bc2dd8bf4fdf43ff9ae81adf4
James-Livesey <jamesliveg@gmail.com>
2024-12-03 19:19:51 +0000

    Allow websocket connections to respond to HTTP requests

--- src/main.js
+++ src/main.js
@@ -5,10 +5,21 @@ var args = parseArgs(Deno.args, {
     default: {port: "8000"}
 });
 
-async function serialiseRequest(request) {
+var handlers = [];
+var nextRequestId = 0;
+
+var openRequests = {};
+
+async function serialiseRequest(request, requestId) {
     var requestLine = `${request.method} ${new URL(request.url).pathname} HTTP/1.1`;
-    var headers = [...request.headers.entries()].map((part) => `${part[0]}: ${part[1]}`).join("\r\n");
+
+    var headers = [
+        ...request.headers.entries().filter((entry) => entry[0] != "x-request-id"),
+        ["x-request-id", requestId]
+    ].map((part) => `${part[0]}: ${part[1]}`).join("\r\n");
+
     var headerData = new TextEncoder().encode(`${requestLine}\r\n${headers}\r\n\r\n`);
+
     var body = request.body ? new Uint8Array(await request.arrayBuffer()) : new Uint8Array();
     var data = new Uint8Array(headerData.length + body.length);
 
@@ -24,6 +35,7 @@ function deserialiseResponse(data) {
     var lines = text.split("\r\n");
     var statusLine = lines.shift();
     var headers = new Headers();
+    var requestId = null;
     var statusMatch;
     var status = null;
     var statusText = null;
@@ -40,19 +52,73 @@ function deserialiseResponse(data) {
 
         var parts = line.split(": ");
 
+        if (parts[0].toLowerCase() == "x-request-id") {
+            requestId = Number(parts[1]);
+
+            continue;
+        }
+
         headers.append(parts[0], parts[1]);
     }
 
-    return new Response(data.slice(bodyOffset), {status, statusText, headers});
+    return {response: new Response(data.slice(bodyOffset), {status, statusText, headers}), requestId};
 }
 
 Deno.serve({port: Number(args.port)}, async function(request) {
-    console.log(new TextDecoder().decode(await serialiseRequest(request)));
+    var url = new URL(request.url);
+
+    if (url.pathname == "/.highway") {
+        if (request.headers.get("upgrade") != "websocket") {
+            return new Response("An `Upgrade` header with the value `\"websocket\"` is required.", {status: 400});
+        }
+
+        if (request.headers.get("authorization") != `Bearer ${args.key}`) {
+            return new Response("Invalid auth token.", {status: 403});
+        }
+
+        var {socket, response} = Deno.upgradeWebSocket(request);
+
+        var handler = function(request, requestId) {
+            socket.send(serialiseRequest(request, requestId));
+        };
+
+        socket.addEventListener("open", function() {
+            handlers.push(handler);
+        });
+
+        socket.addEventListener("message", function(event) {
+            var {requestId, response} = deserialiseResponse(event.data);
+
+            openRequests[requestId]?.(response);
+
+            delete openRequests[requestId];
+        });
+
+        socket.addEventListener("close", function() {
+            handlers = handlers.filter((item) => item != handler);
+        });
+
+        return response;
+    }
+
+    console.log(new TextDecoder().decode(await serialiseRequest(request, 0)));
+
+    if (handlers.length > 0) {
+        var randomHandler = handlers[Math.floor(Math.random() * handlers.length)];
+
+        return new Promise(function(resolve, reject) {
+            var requestId = nextRequestId++;
+
+            openRequests[requestId] = resolve;
+
+            randomHandler(request, requestId);
+        });
+    }
 
     return deserialiseResponse(new TextEncoder().encode(`HTTP/1.1 200 OK\r
 Content-Length: 13\r
 Content-Type: text/plain\r
 \r
 Hello, world!
-`));
+`)).response;
 });
